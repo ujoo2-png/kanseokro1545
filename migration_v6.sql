ef077e7adb001fa37b4c33d8f4e785d42523d5c5
ujoo2-png <ujoo2@hotmail.com>
2026-06-24 14:11:59 +0900

    v1.15.6-2
    v1.15.6   중복 세대 등록 방지 재수정

--- migration_v6.sql
+++ migration_v6.sql
@@ -20,6 +20,19 @@ DROP POLICY IF EXISTS "anon_select_notices" ON notices;
 DROP POLICY IF EXISTS "anon_select_inquiries" ON inquiries;
 DROP POLICY IF EXISTS "anon_select_units" ON units;
 
+-- 기존 anon_all_* 정책이 있으면 먼저 제거 (재실행 안전)
+DROP POLICY IF EXISTS "anon_all_users" ON users;
+DROP POLICY IF EXISTS "anon_all_bills" ON bills;
+DROP POLICY IF EXISTS "anon_all_payments" ON payments;
+DROP POLICY IF EXISTS "anon_all_notices" ON notices;
+DROP POLICY IF EXISTS "anon_all_inquiries" ON inquiries;
+DROP POLICY IF EXISTS "anon_all_units" ON units;
+DROP POLICY IF EXISTS "anon_all_buildings" ON buildings;
+DROP POLICY IF EXISTS "anon_all_contracts" ON contracts;
+DROP POLICY IF EXISTS "anon_all_meters" ON meters;
+DROP POLICY IF EXISTS "anon_all_prepaids" ON prepaids;
+DROP POLICY IF EXISTS "anon_all_deposit_deductions" ON deposit_deductions;
+
 -- 모든 테이블에 anon ALL 정책 (SELECT/INSERT/UPDATE/DELETE 모두 허용)
 CREATE POLICY "anon_all_users" ON users FOR ALL USING (true);
 CREATE POLICY "anon_all_bills" ON bills FOR ALL USING (true);
